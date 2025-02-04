2c73519be8d8ea4887bef2a35f36b9bca7a38808
Niranjan <niranjanshah474@gmail.com>
2025-02-04 20:56:31 +1100

    walls and gates done using bfs

--- wallsAndGates.py
+++ wallsAndGates.py
@@ -97,3 +97,92 @@ class Solution:
 
             # increasing the distance for next levels
             dist += 1
+
+
+
+
+
+"""
+
+Submissions
+Submissions
+
+
+Code
+Testcase
+Test Result
+Test Result
+286. Walls and Gates
+Solved
+Medium
+Topics
+Companies
+You are given an m x n grid rooms initialized with these three possible values.
+
+-1 A wall or an obstacle.
+0 A gate.
+INF Infinity means an empty room. We use the value 231 - 1 = 2147483647 to represent INF as you may assume that the distance to a gate is less than 2147483647.
+Fill each empty room with the distance to its nearest gate. If it is impossible to reach a gate, it should be filled with INF.
+
+ 
+
+Example 1:
+
+
+Input: rooms = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
+Output: [[3,-1,0,1],[2,2,1,-1],[1,-1,2,-1],[0,-1,3,4]]
+Example 2:
+
+Input: rooms = [[-1]]
+Output: [[-1]]
+ 
+
+Constraints:
+
+m == rooms.length
+n == rooms[i].length
+1 <= m, n <= 250
+rooms[i][j] is -1, 0, or 231 - 1.
+
+"""
+
+class Solution2:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        m = len(rooms)
+        n = len(rooms[0])
+
+        queue = deque()
+        # directions to go four ways from a position
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        # storing the locations of gates
+        for r in range(m):
+            for c in range(n):
+                if rooms[r][c] == 0:
+                    queue.append((r,c))
+
+        distance = 0
+        while queue:
+
+            for _ in range(len(queue)):
+                r,c = queue.popleft()
+
+                
+
+                if  rooms[r][c] == 2**31 - 1:
+                    rooms[r][c] = distance
+
+                # going through all the possible paths
+                for dr, dc in directions:
+                    curr_r = r + dr
+                    curr_c = c + dc
+
+                    if ((0 <= curr_r < m) and
+                    (0 <= curr_c < n) and
+                    rooms[curr_r][curr_c] == 2**31 - 1):
+                        queue.append((curr_r,curr_c))
+
+            distance +=1
+
