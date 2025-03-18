55f2c7bcf8ec57434c996c988fc38b6178691594
Niranjan Shah <niranjanshah474@gmail.com>
2025-03-18 23:08:31 +1100

    2401 done using brute force sliding window

---
+++ longestNiceSubarray.py
@@ -0,0 +1,68 @@
+"""
+2401. Longest Nice Subarray
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given an array nums consisting of positive integers.
+
+We call a subarray of nums nice if the bitwise AND of every pair of elements that are in different positions in the subarray is equal to 0.
+
+Return the length of the longest nice subarray.
+
+A subarray is a contiguous part of an array.
+
+Note that subarrays of length 1 are always considered nice.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,8,48,10]
+Output: 3
+Explanation: The longest nice subarray is [3,8,48]. This subarray satisfies the conditions:
+- 3 AND 8 = 0.
+- 3 AND 48 = 0.
+- 8 AND 48 = 0.
+It can be proven that no longer nice subarray can be obtained, so we return 3.
+Example 2:
+
+Input: nums = [3,1,5,11,13]
+Output: 1
+Explanation: The length of the longest nice subarray is 1. Any subarray of length 1 can be chosen.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+"""
+
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        # Time: O(n^2)
+        # Space: O(1)
+        l = 0
+        res = 1
+
+        for r in range(1,len(nums)):
+            
+
+            while l < r:
+                conflict_found = False    
+                for i in range(l,r):
+                    if nums[i] & nums[r] != 0:
+                        conflict_found = True
+                        break
+                # if there is no conflict we break out from the current loop and increase the window
+                if not conflict_found:
+                    break
+                # if there is a conflict, we shrink the window from left, and check for conflict in the remaining window
+                l+=1
+      
+    
+            res = max(res, r-l+1)
+
+        return res
+
